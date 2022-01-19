4ad6c121a31cd747a022a8b87e9ee2c7fdd12278
Furkan Kalkan <furkankalkan@mantis.com.tr>
2022-01-19 17:54:49 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="change-event-service",
-    version="2.0.2",
+    version="2.0.3",
     author="Ramazan Çetin",
     author_email="lramazancetinl@gmail.com",
     description="Change Event Service",
