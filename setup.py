51855bcef43bc62329e032a427bf488463b65268
Furkan Kalkan <furkankalkan@mantis.com.tr>
2022-01-19 18:35:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="change-event-service",
-    version="2.0.4",
+    version="2.0.5",
     author="Ramazan Çetin",
     author_email="lramazancetinl@gmail.com",
     description="Change Event Service",
@@ -24,8 +24,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8"
     ],
     install_requires=['Flask~=2.0.2', 'flask-smorest~=0.36.0', 'marshmallow~=3.14.1', 'pyctuator~=0.18.1',
-                      'Flask-SQLAlchemy==2.5.1', 'SQLAlchemy-Utils~=0.38.2', 'Flask-APScheduler==1.12.3',
-                      'pika~=1.2.0', 'python-slugify~=5.0.2', 'requests~=2.27.1'],
+                      'Flask-SQLAlchemy==2.5.1', 'SQLAlchemy-Utils~=0.38.2', 'psycopg2-binary~=2.9.3',
+                      'Flask-APScheduler==1.12.3','pika~=1.2.0', 'python-slugify~=5.0.2', 'requests~=2.27.1'],
     python_requires=">3.8.*, <4",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"}
