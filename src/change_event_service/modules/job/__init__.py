f81ff84eb4a632f83952243edc3ee6f80688584f
Furkan Kalkan <furkankalkan@mantis.com.tr>
2022-01-27 18:45:03 +0300

    camelCase field name support added & job bugfixes

--- src/change_event_service/modules/job/__init__.py
+++ src/change_event_service/modules/job/__init__.py
@@ -5,6 +5,8 @@ from change_event_service.modules.rest.models import ChangeEventModel
 
 
 def consume_change_events(app_context):
+    print('Job Started')
+    app_context.app_context().push()
     mq_host = app_context.config.get('MQ_HOST')
     mq_vhost = app_context.config.get('MQ_VHOST')
     mq_user = app_context.config.get('MQ_USER')
@@ -13,21 +15,31 @@ def consume_change_events(app_context):
     mq_exchange = app_context.config.get('MQ_EXCHANGE')
     mq_routing_key = app_context.config.get('MQ_ROUTING_KEY')
 
+    def camel_to_snake(s):
+        return ''.join(['_'+c.lower() if c.isupper() else c for c in s]).lstrip('_')
+
     def on_message(channel, method_frame, header_frame, body):
-        app_context.logger.debug(f'Received message: {body}')
+        body = body.decode('utf-8')
+        change_event = None
         try:
-            _body = json.loads(body)
+            body = json.loads(body)
+            _body = {}
+            for field in body:
+                _body[camel_to_snake(field)] = body[field]
             change_event = ChangeEventModel(**_body)
             db.session.add(change_event)
             db.session.commit()
         except Exception as e:
-            app_context.logger.error(f'Error: {e}')
-            db.session.rollback()
+            print(e)
+            body = json.dumps(body).encode('utf-8')
             channel.basic_publish(exchange=mq_exchange, routing_key=mq_routing_key, body=body)
+            db.get_app()
+            db.session.rollback()
         finally:
             channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
     cred = pika.PlainCredentials(mq_user, mq_pass)
     conn = pika.BlockingConnection(pika.ConnectionParameters(host=mq_host, virtual_host=mq_vhost, credentials=cred))
     channel = conn.channel()
-    channel.basic_consume(mq_queue, on_message)
+    channel.basic_consume(mq_queue,  on_message, consumer_tag='change-event-service')
+    channel.start_consuming()
