d7f691d3c1f370fa92654178c00833dc38d00511
Furkan Kalkan <furkankalkan@mantis.com.tr>
2022-10-07 11:35:47 +0300

    Ignore extra data from json other than column name when pass to model.

--- src/change_event_service/modules/rest/models.py
+++ src/change_event_service/modules/rest/models.py
@@ -5,6 +5,12 @@ from sqlalchemy_utils import ScalarListType
 
 
 class ChangeEventModel(db.Model):
+    def __init__(self, **entries):
+        """Override to avoid TypeError when passed spurious column names"""
+        col_names = set([col.name for col in self.__table__.columns])
+        superentries = {k: entries[k] for k in col_names.intersection(entries.keys())}
+        super().__init__(**superentries)
+
     __tablename__ = os.environ.get('DB_TABLE_NAME')
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     ip = db.Column(db.String)
